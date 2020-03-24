2c838d710a3f3c2b9afa5a393915bcc1a18ec7b9
Janar <jogijanar@gmail.com>
2020-03-24 20:54:09 +0200

    Rework game level start to separate function

--- src/Game.ts
+++ src/Game.ts
@@ -10,6 +10,7 @@ import { RenderSystem } from './systems/RenderSystem'
 import { System } from './systems/System'
 import { TransformSystem } from './systems/TransformSystem'
 import { WeaponSystem } from './systems/WeaponSystem'
+import { Dimensions } from './Math'
 
 export class Game {
     readonly isDebug = true
@@ -17,42 +18,46 @@ export class Game {
     readonly interval = 1000 / this.fps
     then: number = performance.now()
 
-    readonly entities: EntityManager
     readonly levelManager: LevelManager
-
-    readonly systems: System[]
+    readonly canvas: HTMLCanvasElement
+    readonly viewPort: Dimensions
 
     constructor(canvas: HTMLCanvasElement) {
-        const viewport = [canvas.width, canvas.height] as const
-        this.levelManager = new LevelManager(viewport)
-        const level = this.levelManager.getLevel(0)
-        const { mapSize, entities, stars } = level
-        this.entities = entities
-        this.systems = [
-            new ControlSystem(this.entities, canvas),
-            new MovementSystem(this.entities),
-            new TransformSystem(this.entities, mapSize),
-            new CollisionSystem(this.entities),
-            new WeaponSystem(this.entities),
-            new HealthSystem(this.entities),
-            new CameraSystem(this.entities, viewport, mapSize),
-            new HubSystem(this.entities),
-            // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-            new RenderSystem(this.entities, canvas.getContext('2d')!, stars, this.isDebug),
-        ]
-        level.init()
+        this.viewPort = [canvas.width, canvas.height] as const
+        this.canvas = canvas
+        this.levelManager = new LevelManager(this.viewPort)
     }
 
-    gameLoop() {
-        requestAnimationFrame(() => this.gameLoop())
+    start(levelNo: number) {
+        const level = this.levelManager.getLevel(levelNo)
+        const { mapSize, entities, stars } = level
+        const systems = [
+            new ControlSystem(entities, this.canvas),
+            new MovementSystem(entities),
+            new TransformSystem(entities, mapSize),
+            new CollisionSystem(entities),
+            new WeaponSystem(entities),
+            new HealthSystem(entities),
+            new CameraSystem(entities, this.viewPort, mapSize),
+            new HubSystem(entities),
+            // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+            new RenderSystem(entities, this.canvas.getContext('2d')!, stars, this.isDebug),
+        ]
+
+        level.init()
+        this.gameLoop(entities, systems)
+    }
+
+    gameLoop(entities: EntityManager, systems: System[]) {
+        requestAnimationFrame(() => this.gameLoop(entities, systems))
 
         const now = performance.now()
         const delta = now - this.then
 
         if (delta > this.interval) {
             this.then = now - (delta % (this.interval))
-            this.entities.proccess(this.systems)
-            this.entities.clean()
+            entities.proccess(systems)
+            entities.clean()
         }
     }
 }
--- src/Main.ts
+++ src/Main.ts
@@ -4,5 +4,5 @@ window.onload = () => {
     const canvas = document.getElementById('canvas') as HTMLCanvasElement
     const game: Game = new Game(canvas)
 
-    game.gameLoop()
+    game.start(0)
 }
